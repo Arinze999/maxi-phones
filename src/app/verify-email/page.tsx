71b202374bb558545933ff4252c5121d17f69cad
Arinze999 <akanagapyt@gmail.com>
2025-05-11 01:47:09 +0100

    fixx 3

--- src/app/verify-email/page.tsx
+++ src/app/verify-email/page.tsx
@@ -3,17 +3,17 @@ export const dynamic = 'force-dynamic';
 
 import React from 'react';
 import FadeLoader from 'react-spinners/FadeLoader';
-import { useVerifyEmail } from '@/hooks/auth/useVerifyEmail';
+// import { useVerifyEmail } from '@/hooks/auth/useVerifyEmail';
 
 const VerifyEmailPage = () => {
-  const { loading } = useVerifyEmail();
+  // const { loading } = useVerifyEmail();
 
   return (
     <div className="flex flex-col gap-10 items-center justify-center min-h-screen">
       <p className="text-center text-[14px]">Verifying Email...please wait</p>
-      {loading && (
+      {/* {loading && (
         <FadeLoader loading={true} color="#db4444" speedMultiplier={1.5} />
-      )}
+      )} */}
     </div>
   );
 };
